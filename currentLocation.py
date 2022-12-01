4b050be7e3e9c8f53e65b770b0cb8c2235a7f258
dimonoid <dimonoid123@gmail.com>
2022-11-30 19:51:19 -0500

    Cleanup

--- currentLocation.py
+++ currentLocation.py
@@ -4,23 +4,24 @@ from random import randrange
 
 geoLoc = Nominatim(user_agent="GetLoc")
 
+
 def getLocation():
-    latitude = 45.419510 
+    latitude = 45.419510
     longitude = -75.678770
     for i in range(10):
-        result = str(latitude)+", "+str(longitude)
+        result = str(latitude) + ", " + str(longitude)
         locname = geoLoc.reverse(result)
         print(locname.address)
-        latitude+=1
-        longitude+=1
-        i=i-1
+        latitude += 1
+        longitude += 1
+        i = i - 1
     sleep(2)
     return locname
-    
+
 
 def displayL():
-    latitude = 45.419510 
+    latitude = 45.419510
     longitude = -75.678770
-    displayResult = str(latitude)+", "+str(longitude)  
+    displayResult = str(latitude) + ", " + str(longitude)
     displayLocname = geoLoc.reverse(displayResult)
     return displayLocname
--- dbFunc.py
+++ dbFunc.py
@@ -9,15 +9,16 @@ from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
-#set name of database for code
+# set name of database for code
 db_name = 'Database.db'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-#initialize database, used for SQLAlchemy commands
+# initialize database, used for SQLAlchemy commands
 db = SQLAlchemy(app)
+
 
 class Plate(db.Model):
     __tablename__ = 'LicensePlates'
@@ -25,15 +26,17 @@ class Plate(db.Model):
     Owner = db.Column(db.String, nullable=False)
     Info = db.Column(db.String, nullable=False)
 
+
 class Criminal(db.Model):
     __tablename__ = 'Criminals'
     Name = db.Column(db.String, primary_key=True, nullable=False, unique=True)
     Crime = db.Column(db.String, nullable=False)
 
-#compare string detected from license plate to plate table in database
+
+# compare string detected from license plate to plate table in database
 def plate_detected(str):
     try:
-        plates = Plate.query.filter_by(row[0]==str).all()
+        plates = Plate.query.filter_by(row[0] == str).all()
         plate_text = '<ul>'
         for plate in LicensePlates:
             plate_text += '<li>' + plate.LicensePlate + ', ' + plate.Owner + ', ' + plate.Info + '<li>'
@@ -42,11 +45,12 @@ def plate_detected(str):
     except Exception as e:
         error_text = "<p>Plate not found or other error </p>"
         return error_text
-    
-#compare string of person's name to database
+
+
+# compare string of person's name to database
 def compare_face(str):
     try:
-        criminals = Criminal.query.filter_by(row[0]==str).all()
+        criminals = Criminal.query.filter_by(row[0] == str).all()
         criminal_text = '<ul>'
         for criminal in Criminals:
             criminal_text += '<li>' + criminal.Name + ', ' + criminal.Crime + '<li>'
@@ -56,22 +60,25 @@ def compare_face(str):
         error_text = "<p>Criminal not found or other error </p>"
         return error_text
 
-#add License Plate to database
-def add_plate(LicensePlate, Owner, Info):
-	try:
-		con = sql.connect('Database.db')
-		c = con.cursor()
-		c.execute("INSERT INTO LicensePlate (LicensePlate, Owner, Info) VALUES (%s, %s, %s)" %(LicensePlate, Owner, Info))
-		con.commit()
-	except:
-		print("Error adding plate to db")
 
-#add Face to database
+# add License Plate to database
+def add_plate(LicensePlate, Owner, Info):
+    try:
+        con = sql.connect('Database.db')
+        c = con.cursor()
+        c.execute(
+            "INSERT INTO LicensePlate (LicensePlate, Owner, Info) VALUES (%s, %s, %s)" % (LicensePlate, Owner, Info))
+        con.commit()
+    except:
+        print("Error adding plate to db")
+
+
+# add Face to database
 def add_face(Name, Crime):
-	try:
-		con=sql.connect('Database.db')
-		c=con.cursor()
-		c.execute("INSERT INTO Criminals (Name, Crime) VALUES (%b, %s, %s)" %(Name, Crime))
-		con.commit()
-	except:
-		print("Error adding face to db")
+    try:
+        con = sql.connect('Database.db')
+        c = con.cursor()
+        c.execute("INSERT INTO Criminals (Name, Crime) VALUES (%b, %s, %s)" % (Name, Crime))  # TODO
+        con.commit()
+    except:
+        print("Error adding face to db")
