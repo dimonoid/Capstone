ddd6009b637d27f0992b6e244b731464cb70e53e
dimonoid <dimonoid123@gmail.com>
2023-04-04 16:51:00 -0400

    Offline resources. TODO something with different versions of jquery. Added favicon.ico

--- app.py
+++ app.py
@@ -151,6 +151,13 @@ def gen_frames(debug=False, filename=None):
             break
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
+
+
 def log_person():
     return currentName
 
